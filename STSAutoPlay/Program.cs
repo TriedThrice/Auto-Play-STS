8177f290ef52cdc733a2153a9ee8c5e475ef6bbb
TriedThrice <justingreen24@gmail.com>
2026-08-20 14:00:14 +1000

    Initial Commit

---
+++ STSAutoPlay/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace STSAutoPlay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Thread.Sleep(1000);
+            char key = Console.ReadKey().KeyChar;
+            Console.WriteLine("You pressed: " + key);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+
+        }
+    }
+}
